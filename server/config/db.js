6e3a0956846a9b6a205ad4132d3db503ce393b72
rdeguzman <ruthjf.16@gmail.com>
2022-10-27 12:48:15 -0400

    Final Commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports =
+ {
+ "URI":"mongodb+srv://rdeguzman:ys9Keo6MMTPltnZQ@rdeguzman.aar48eg.mongodb.net/book_store?retryWrites=true&w=majority",
+ Secret: "SomeSecret",
+};
+//ys9Keo6MMTPltnZQ
---
+++ server/routes/contact.js
@@ -0,0 +1,38 @@
+let express = require("express");
+let router = express.Router();
+let mongoose = require("mongoose");
+
+let jwt = require("jsonwebtoken");
+
+let passport = require("passport");
+
+let contactController = require("../controllers/contact");
+
+// helper function for guard purposes
+function requireAuth(req, res, next) {
+  // check if the user is logged in
+  if (!req.isAuthenticated()) {
+    return res.redirect("/login");
+  }
+  next();
+}
+
+/* GET Route for the Contact List page - READ Operation */
+router.get("/", contactController.displayContactList);
+
+/* GET Route for displaying the Add page - CREATE Operation */
+router.get("/add", requireAuth, contactController.displayAddPage);
+
+/* POST Route for processing the Add page - CREATE Operation */
+router.post("/add", requireAuth, contactController.processAddPage);
+
+/* GET Route for displaying the Update page - UPDATE Operation */
+router.get("/update/:id", requireAuth, contactController.displayUpdatePage);
+
+/* POST Route for processing the Update page - UPDATE Operation */
+router.post("/update/:id", requireAuth, contactController.processUpdatePage);
+
+/* GET to perform  Deletion - DELETE Operation */
+router.get("/delete/:id", requireAuth, contactController.performDelete);
+
+module.exports = router;
